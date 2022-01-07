a625b1896a11132986196deb68bf15657b473911
blackb1rd <blackb1rd.mov@gmail.com>
2022-01-07 23:06:02 +0700

    linux only

--- brutehttp.c
+++ brutehttp.c
@@ -15,6 +15,7 @@
 */
 
 #include <stdio.h>
+#include <resolv.h>
 #include <stdlib.h>
 #include <stdint.h>
 #include <string.h>
@@ -29,7 +30,7 @@ char* gethost(char* url);
 
 int main(int argc,const char **argv)
 {
-  if (argc < 3 || argc > 3) {
+  if (argc < 3 || argc > 4) {
     usage();
     return 1;
   }
@@ -39,16 +40,23 @@ int main(int argc,const char **argv)
   FILE *fp;
   struct hostent *hent;
   const char *url = argv[1];
-  const char *USERAGENT  = "Brutehttp 0.5";
+  const char *method = argv[3];
+  if (method == NULL) {
+    method = "GET";
+  }
+  const char *USERAGENT  = "Brutehttp 1.0";
   const int  PORT        = 80;
 
   head = malloc(2048);
 
   char *hostname = gethost((char*)url);
-  printf("host: %s\n", hostname);
   hent = gethostbyname(hostname);
+  printf("Hostname: %s\n", hent->h_name);
+  printf("Address type #: %d\n", hent->h_addrtype);
+  printf("Address length: %d\n", hent->h_length);
+  printf("Method: %s\n", method);
 
-  if (inet_ntop(AF_INET, (void *)hent->h_addr_list[0], ip, INET_ADDRSTRLEN) == NULL) {
+  if (inet_ntop(AF_INET, (void *)hent->h_addr, ip, INET_ADDRSTRLEN) == NULL) {
     perror("Can't resolve hostname");
     return 1;
   }
@@ -59,7 +67,6 @@ int main(int argc,const char **argv)
   target.sin_family = AF_INET;
   target.sin_port = htons (PORT);
 
-  char page[71];
   char buf[BUFSIZ];
   int sock_tcp;
   unsigned int rescode;
@@ -69,17 +76,21 @@ int main(int argc,const char **argv)
   printf("--------------------------------------------------------------------------------\n");
   fp = fopen(argv[2], "r");
 
-  while (fgets(page,70,fp) != NULL) {
+  char *line = NULL;
+  size_t len = 0;
+  ssize_t read;
+  while ((read = getline(&line, &len, fp)) != -1) {
+    if ((line)[read - 1] == '\n') {
+      (line)[read - 1] = '\0';
+    }
     //create socket TCP
     if ((sock_tcp = socket (AF_INET , SOCK_STREAM , IPPROTO_TCP)) < 0) {
       perror("Could not create socket TCP");
       return 1;
     }
 
-    if (page[strlen(page)-1] == '\n')
-      page[strlen(page)-1] = '\0';
-    printf("\r%-70s", page);
-    snprintf(head, 2048, "HEAD /%s HTTP/1.1\r\nUser-Agent: %s\r\nHost: %s\r\nAccept: */*\r\nConnection: close\r\n\r\n", page, USERAGENT, url);
+    //printf("\r%-70s", line);
+    snprintf(head, 2048, "%s /%s HTTP/1.1\r\nUser-Agent: %s\r\nHost: %s\r\nAccept: */*\r\nConnection: close\r\n\r\n", method, line, USERAGENT, url);
     if (connect(sock_tcp, (struct sockaddr *)&target, sizeof(target)) < 0) {
       perror("Could not connect");
       return 1;
@@ -88,8 +99,7 @@ int main(int argc,const char **argv)
     recv(sock_tcp, buf, BUFSIZ, 0);
 
     sscanf(buf, "HTTP/1.%*[^ ] %3u[^ ]", &rescode);
-    if (rescode != 404)
-      printf("\r| %-70s| %d |\n", page, rescode);
+    printf("\r| %-70s| %d |\n", line, rescode);
   }
   printf("\r-------------------------------------------------------------------------------\n");
 
